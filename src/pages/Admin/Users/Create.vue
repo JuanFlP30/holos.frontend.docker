d7d83b69a091d23f1edabba24d1d3b43787f4cfb
Moisés de Jesús Cortés Castellanos <ing.moisesdejesuscortesc@notsoweb.com>
2024-12-19 16:50:43 -0600

    FIX: Creación de usuario

--- src/pages/Admin/Users/Create.vue
+++ src/pages/Admin/Users/Create.vue
@@ -2,7 +2,7 @@
 import { onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { api, useForm } from '@Services/Api';
-import { transl, viewTo } from './Module';
+import { apiTo, transl, viewTo } from './Module';
 
 import IconButton      from '@Holos/Button/Icon.vue'
 import Input           from '@Holos/Form/Input.vue';
